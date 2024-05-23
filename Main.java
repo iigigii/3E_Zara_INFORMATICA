ba92cb345d16cb4b23128ac98eb9d9dc3ffe08a8
iigigii <150442603+iigigii@users.noreply.github.com>
2024-05-23 21:31:33 +0200

    Add files via upload
    esercizi by raspa

--- Main.java
+++ Main.java
@@ -1,32 +1,48 @@
 public class Main {
+
     public static void main(String[] args) {
-        String input = "la mamma di Martina va al mercato a comprare le fragole per fare la macedonia";
-        splitString(input, 40);
-    }
-
-
-
-    public static void splitString(String input, int maxLength) {
-        int length = input.length();
-        int startIndex = 0;
-        int endIndex = 0;
-
-        while (startIndex < length) {
-            endIndex = Math.min(startIndex + maxLength, length);
-            while (endIndex < length && !Character.isWhitespace(input.charAt(endIndex - 1))) {
-                endIndex--;
-            }
-
-            System.out.println(input.substring(startIndex, endIndex));
-            startIndex = endIndex;
-            while (startIndex < length && Character.isWhitespace(input.charAt(startIndex))) {
-                startIndex++;
-            }
+        int[] array = {3, 4, 2, 5, 6, 1, 7, 8, 9};
+        evenOddSearch(array);
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
         }
     }
 
- 
+    public static void evenOddSearch(int[] arr) {
+        int n = arr.length;
+        int l = 0, r = n - 1;
+        while (l < r) {
+            while (arr[l] % 2 == 0 && l < r)
+                l++;
+            while (arr[r] % 2 == 1 && l < r)
+                r--;
+            if (l < r) {
+                int temp = arr[l];
+                arr[l] = arr[r];
+                arr[r] = temp;
+            }
+        }
 
+        // Ordina i numeri pari in modo crescente
+        for (int i = 0; i < l; i++) {
+            for (int j = i + 1; j < l; j++) {
+                if (arr[j] < arr[i]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
 
-
+        // Ordina i numeri dispari in modo decrescente
+        for (int i = l; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] > arr[i]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
 }
